fa9e2656c165279a56509e6f52549c804e199911
Katie Rideout <rideout.km@gmail.com>
2020-01-26 00:29:13 -0600

    ooops
    copy and pasted example... here's mine

--- snow creature/snowman.js
+++ snow creature/snowman.js
@@ -1,6 +1,6 @@
 let grid = undefined;
-let stroke1 = prompt("enter a basic color name in lower case", "gray");
-let stroke2 = prompt("enter another basic color name in lower case", "purple");
+//let stroke1 = prompt("enter a basic color name in lower case", "gray");
+//let stroke2 = prompt("enter another basic color name in lower case", "purple");
 function setup (){
 	createCanvas(1000, 800);
 	background("#ccc");
@@ -9,10 +9,16 @@ function setup (){
 
 function draw(){
 	background(grid);
+	//arms
+	stroke("black");
+	strokeWeight(20);
+	line(350,400,200,200);
+	line(650,400,800,200);
 	//snowman legs
 	fill("#f1f1f1");
-	stroke(stroke1);
-	strokeWeight(20);
+	stroke("gray");
+	strokeWeight(15);
+	
 	//left leg
 	ellipse(350, 650, 200);
 	//right leg
@@ -22,19 +28,26 @@ function draw(){
 	//head
 	ellipse(500, 200, 200);
 	//hat brim
-	stroke(stroke2);
+	stroke("purple");
 	strokeWeight(40);
 	line(400,120,600,120);
 	//hat body
 	quad(400, 50, 600, 50, 550, 120, 450, 120);
 	//eyes
 	stroke(0);
-	strokeWeight(50);
-	point(425, 200);
-	point(575, 200);
+	strokeWeight(20);
+	point(460, 180);
+	point(540, 180);
 	//mouth
 	noFill();
 	strokeWeight(10);
-	arc(500, 200, 80, 80, 0, HALF_PI);
+	arc(500, 220, 70, 50, 0, PI);
+	//buttons
+	stroke(0);
+	strokeWeight(35);
+	point(500,450);
+	point(500,500);
+	point(500,400);
+	
 	
 }
