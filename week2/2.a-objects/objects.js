a3df92918054a00bfa3269fb9109246d69965030
Katie Rideout <rideout.km@gmail.com>
2020-02-02 23:44:34 -0600

    begin files for week 2: 2a

---
+++ week2/2.a-objects/objects.js
@@ -0,0 +1,8 @@
+
+const myQuiz=[
+	{question: 'Who is the second most nominated person in Academy Awards history behind Walt Disney?', answer: 'John Williams (composer)'}
+	{question: 'Which film director has John Williams composed the most original scores for?', answer: 'Steven Spielberg'}
+	
+]
+
+const firstQuestion = window.prompt(myQuiz)
