1fecb9ae5260e271d9825f1431447d8526efd97d
Katie Rideout <rideout.km@gmail.com>
2020-02-03 00:00:29 -0600

    steps

--- week2/2.a-objects/objects.js
+++ week2/2.a-objects/objects.js
@@ -1,8 +1,11 @@
 
-const myQuiz=[
-	{question: 'Who is the second most nominated person in Academy Awards history behind Walt Disney?', answer: 'John Williams (composer)'}
+const myQuiz= [
+	
+	{question: 'Who is the second most nominated person in Academy Awards history behind Walt Disney?', answer: 'John Williams (composer)'},
 	{question: 'Which film director has John Williams composed the most original scores for?', answer: 'Steven Spielberg'}
 	
-]
+];
 
-const firstQuestion = window.prompt(myQuiz)
+const myRandomIndex = myQuiz[Math.round(Math.random() * myQuiz.Length - 1)];
+let userAnswer = prompt(myRandomIndex.question, '');
+window.alert('Your answer was' + ' ' + userAnswer + '. ' + 'The correct answer is' + ' ' + myRandomIndex.answer + '. ')
