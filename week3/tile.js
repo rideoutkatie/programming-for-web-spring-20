1f01a1e8aa80402be1f2af511a62c492be8e6f70
Katie Rideout <rideout.km@gmail.com>
2020-02-06 22:23:30 -0600

    building tile

--- week3/tile.js
+++ week3/tile.js
@@ -1,12 +1,27 @@
 function setup() {
-  createCanvas(480,120);
+  createCanvas(200,200);
+}
+
+function createTile() {
+	translate(0, 0);
+	fill('blue');
+	rect(0, 0, 200, 200);
+	stroke('white');
+	strokeWeight(5);
+	line(0, 0, 200, 200);
+	line(200, 0, 0, 200);
+	noStroke();
+	fill('yellow');
+	circle(100, 50, 50);
+	circle(50, 100, 50);
+	circle(100, 150, 50);
+	circle(150, 100, 50);
+	noFill();
+	stroke('purple');
+	strokeWeight(5);
+	circle(100, 100, 100);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-		fill(0);
-	} else {
-		fill(255);
-	} 
-	ellipse(mouseX,mouseY, 80, 80);
+	createTile();
 }
