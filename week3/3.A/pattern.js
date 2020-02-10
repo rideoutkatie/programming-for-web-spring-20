4fa6f35c32f7adb54e61532be2c4646d653b3410
Katie Rideout <rideout.km@gmail.com>
2020-02-09 23:53:53 -0600

    changed file name

--- week3/3.A/pattern.js
+++ week3/3.A/pattern.js
@@ -1,72 +1,40 @@
+let rotateBy=30;
 function setup() {
-  createCanvas(600,600);
+	createCanvas(600, 600);
+	background(255);
+	angleMode(DEGREES);
+	
 }
 
-function createTile(originX, originY, primaryColor, secondaryColor) {
-	translate(originX, originY);
-	fill(primaryColor);
-	rect(0, 0, 200, 200);
-	stroke('white');
-	strokeWeight(5);
-	line(0, 0, 200, 200);
-	line(25, 0, 200, 175);
-	line(50, 0, 200, 150);
-	line(75, 0, 200, 125);
-	line(100, 0, 200, 100);
-	line(125, 0, 200, 75);
-	line(150, 0, 200, 50);
-	line(175, 0, 200, 25);
-	strokeWeight(5);
-	line(200, 0, 0, 200);
-	
-	noStroke();
-	fill(secondaryColor);
-	circle(25, 0, 20);
-	circle(50, 0, 20);
-	circle(75, 0, 20);
-	circle(100, 0, 20);
-	circle(125, 0, 20);
-	circle(150, 0, 20);
-	circle(175, 0, 20);
-	circle(0, 25, 20);
-	circle(0, 50, 20);
-	circle(0, 75, 20);
-	circle(0, 100, 20);
-	circle(0, 125, 20);
-	circle(0, 150, 20);
-	circle(0, 175, 20);
-	
-fill(primaryColor)
-	stroke(secondaryColor);
-	strokeWeight(5);
-	circle(200, 200, 230);
-	fill(secondaryColor)
-	stroke(secondaryColor)
-	circle(200, 200, 180);
-	fill(primaryColor)
-	stroke(secondaryColor);
-	circle(200, 200, 130);
-	fill(secondaryColor)
-	stroke(secondaryColor)
-	circle(200, 200, 80);
-	
-	
-	
-
-
+function makeArm(rotateBy){
+	alt = Math.round(rotateBy / 360)
+	console.log(alt);
+	noFill();
+	stroke('#FDF6E5');
+	strokeWeight(1);
+	rect(30, 10, 1 + alt, 1 * alt);	
+	stroke('#ffcccc')
+	ellipse(40, 40, 1 + alt, 1 * alt);
+	stroke('#f2ffcc')
+	bezier(50, 50, 50, 50, 200 - alt, 100 - alt, 10, 10);
+	stroke('#FAF6FE');
+	bezier(100, 100, 100, 100, 400 - alt, 200 - alt, 100, 100);
+	stroke('#A6A9FE');
+	strokeWeight(.3);
+	rect(10, 10, 10 * alt, 1 - alt);	
+	stroke('#2EBA8B');
+	bezier(50, 100, 100, 100, 400 / alt, 200 / alt, 100, 100);
+	stroke('#F8BC36');
+	bezier(50, 10, 10, 10, 10 * alt, 10 / alt, 200, 100);
 }
 
 function draw() {
-	createTile(0, 0, '#E74C3C', 'white');
-	createTile(0, 200, '#C70039', 'white');
-	createTile(0, 200, '#900C3F', 'white');
-	
-	createTile(200, -400, 'white', '#E74C3C');
-	createTile(0, 200, 'white', '#C70039');
-	createTile(0, 200, 'white', '#900C3F');
-	
-	createTile(200, -400, '#E74C3C', 'white');
-	createTile(0, 200, '#C70039', 'white');
-	createTile(0, 200, '#900C3F', 'white');
+	translate(300, 300);
+	rotate(rotateBy); 
+	makeArm(rotateBy);
+	rotateBy += 30;
+}
+
+function mousePressed(){
 	noLoop();
 }
