5474a38d9bfbb452bd54bc84f900a3e994eebc85
Katie Rideout <rideout.km@gmail.com>
2020-02-14 09:44:51 -0600

    Update exercise2.js

---
+++ week4/4.2/exercise2.js
@@ -0,0 +1,14 @@
+function setup () {
+	createCanvas(500, 500);
+
+}
+
+function draw () {
+		background(155);
+		drawShape();
+}
+
+function drawShape () {
+	fill('purple');
+	rect(0, 0, 75, 75);
+}
