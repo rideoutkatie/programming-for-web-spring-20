3087ea5d896a5b4c6eabfbd077b525cfe522fda4
Katie Rideout <rideout.km@gmail.com>
2020-02-17 13:21:29 -0600

    grid
    this is following along with class demo and then commenting a lot of it out (clicking, mouse pressed, loops)

--- week4/4.a/4a.js
+++ week4/4.a/4a.js
@@ -1,13 +1,47 @@
-//select empty div and assign it to a variable
-const myEmptyDiv = document.querySelector('#myEmptyDiv');
-//put a h1 inside of it
-//create a new element
-const heading = document.createElement('h1');
-heading.innerHTML = 'Hello class! It is a snowy day!';
-heading.style.cursor = 'pointer';
-heading.addEventListener('click', handleHeadingClick);
-myEmptyDiv.appendChild(heading);
-function handleHeadingClick(){
-	document.body.style.backgroundColor= 'red';
+const cardWidth = 120;
+const cardHeight = 200;
+//let circleX = 200;
+//let circleY = 300;
+//let startingX = 200;
+//let startingY = 100;
+//let startingID = 0;
+
+//let myCircles = [];
+function setup () {
+	createCanvas(1000,500);
+	background(255);
+	stroke(1);
 	
-}
+	//ellipse(circleX, circleY, circleDiameter);
+		//for (let k = 0; k < 2; k++) {
+		//for (let i = 0; i < 4; i++) {
+			//ellipse(startingX, startingY, circleDiameter);
+		//myCircles.push({ x: startingX, y: startingY, id: startingID });
+		//startingX += 150;
+		//startingID++;
+	//}
+			//startingY += 150;
+			//startingX = 200;
+	//}
+
+		//console.log(myCircles);
+	}
+ function draw (){
+	 for (let i = 50; i < 1000; i += 150){
+					 rect(i, 50, cardWidth, cardHeight);
+					 rect(i, 300, cardWidth, cardHeight);
+	 }
+	 
+ }
+//function mousePressed(){
+	//for(let j = 0; j < myCircles.length; j++) {
+			//let distance = dist(mouseX, mouseY, myCircles[j].x, myCircles[j].y);
+			//if (distance < circleDiameter / 2) {
+				//console.log('circle has be clicked', myCircles[j].id);
+	//}
+	
+	//console.log('mouse pressed', mouseX, mouseY);	
+	//if(mouseX> rectX && mouseY < rectX + rectWidth && mouseY > 100  && mouseY < 300) {
+		//console.log('square has been hit');
+	
+//}}
