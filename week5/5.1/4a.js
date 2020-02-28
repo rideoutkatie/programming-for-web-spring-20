4ffab1dba71afc26a9ef16d91412d4c6145b55f7
Katie Rideout <rideout.km@gmail.com>
2020-02-28 17:14:31 -0600

    final commit

---
+++ week5/5.1/4a.js
@@ -0,0 +1,50 @@
+//Timers
+let blockX = 0;
+let blockY = 0;
+let drawTimer;
+let blockColor = "#E33D4F";
+const speed = 100;
+const distance = 3;
+
+function setup () {
+	createCanvas(500, 500);
+	background(0);
+}
+
+function drawBlock(x, y, color) {
+	fill(color || 255);
+	rect(x, y, 50, 50);
+}
+
+function keyTyped() {
+	let keyToNumber = Number(key);
+	if (isNaN(keyToNumber)){
+		return;
+	}
+	
+	keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+	console.log('converted number', keyToNumber);
+	blockColor = keyToNumber;
+}
+
+window.setTimeout(() => {
+	drawTimer = window.setInterval(() => {
+		if (blockY - 50 <= height) {
+					drawBlock(blockX, blockY, blockColor);
+					blockY += distance;
+		} else {
+			blockY = 0;
+			blockX += 50;
+		}
+		if (blockY - 50 > height && blockX - 50 > width) {
+			//cancels timer
+				window.clearInterval(drawTimer);
+				alert('done');
+		}
+})
+
+
+
+	
+	}, speed);
+1500
---
+++ week5/practice-quiz/exercise1.js
@@ -0,0 +1,16 @@
+
+
+/*var kensDogs;
+var kensDogs= [ 'Lucy', 'Lucky', 'Linda', 'Lacey', 'Carl' ]
+
+for (let i = 0; i < kensDogs.length; i++) { 
+	
+console.log("My dog, " + kensDogs[i] + ", is a really good puppy.");
+}*/
+
+
+var myCharacters = [
+	{name: 'barney' 'james' 'aly' 'doc'}
+]
+
+console.log(myCharacters)
