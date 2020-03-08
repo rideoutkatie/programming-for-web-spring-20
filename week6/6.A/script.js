0e1522db9e1028e6cee057ca60f35af968b86b7a
Katie Rideout <rideout.km@gmail.com>
2020-03-08 17:05:03 -0500

    final commit

---
+++ week6/6.A/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+  el: '#llamas',
+  data: {
+		animalOne: 'llamas',
+		animalTwo: 'horses',
+    image: 'img/horse.jpg',
+    evenIf: true,
+    details: ["aren't really meant for riding", "have been used as pack animals for a long time", "goats and raindeer are also pack animals", "can be milked", "typically only spit at other llamas unless they are poorly socialized", "have good endurance for travelling", "llamas look a little bit like a camel and horse... think about it, camels also spit.", "have wool that is VERY soft", "are so cool. I bet you're glad you read this"],
+  
+  },
+  
+
+ }
+)
+
