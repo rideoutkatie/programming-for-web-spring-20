f3433de261edd27cd3b8e33cf173472eff958ade
Katie Rideout <rideout.km@gmail.com>
2020-03-09 11:51:22 -0500

    folders

--- week7/7.2/script.js
+++ week7/7.2/script.js
@@ -1,19 +1,54 @@
-// Add your code here
+
+
+const llamaArray = [{
+	rank: 1,
+	name: "Barack O. Llama"
+},{
+	rank: 2,
+	name: "Llamanade"
+},{
+	rank: 3,
+	name: "Carl"
+},{
+	rank: 4,
+	name: "Shama Llama Ding Dong"
+},{
+	rank: 5,
+	name: "Drama Llama"
+},{
+	rank: 6,
+	name: "No Probllama"
+},{
+	rank: 7,
+	name: "fluffy"
+},{
+	rank: 8,
+	name: "LLAMACORN"
+},{
+	rank: 9,
+	name: "Kuzco"
+},{
+	rank: 10,
+	name: "Llama Bean"
+},{
+	rank: 11,
+	name: "Bahama Llama"
+},{
+	rank: 12,
+	name: "Tina"},
+{
+	rank: 13,
+	name: "Llamanator"
+},{
+	rank: 14,
+	name: "Giraffe Sheep"
+},{
+	rank: 15,
+	name: "Llamabogini"
+}];
 var vm = new Vue({
-  el:"#app",
+  el: "#app",
   data: {
-    myNumbers: [1, 2, 3, 4, 5],
-		animals:[
-			{name: "panda", color:"black"},
-			{name:"dog", color:"brown"},
-			{name:"frog", color:"green"}
-		],
-		myDog: {
-			name:"Harvey",
-			breed:"shih tzu",
-			color:"red",
-			weight:"13"
-			
-	}
+    llamas: llamaArray
   }
 })
