d51b50aa1d8d90f5d8449e64057c997e1c4550ad
Katie Rideout <rideout.km@gmail.com>
2020-04-14 12:03:26 -0500

    final commit
    changed from editing locally to editing from mcad server

--- week10/exercise2.js
+++ week10/exercise2.js
@@ -1,3 +1,25 @@
+Vue.component('streaming-track', {
+	template: `<div :class="['track', trending]">
+			<h3>{{track.title}}</h3>
+			<div><img :src="track.cover" alt=""></div>
+			<small>{{track.artist}}</small>
+	</div>`,
+	props:['track'],
+	computed: {
+		trending: function () {
+			const delta = this.track.rank - this.track.position.positionLastWeek;
+			if(delta > 0) {
+				return 'up'
+			} else if (delta < 0) {
+				return 'down'
+			}
+				else {
+					return 'no-change'
+				}
+			
+		}
+	}
+})
 const vm = new Vue({
 	el: "#musicApp",
 	data: {
@@ -11,5 +33,6 @@ const vm = new Vue({
 			vm.tracks = response.data;
 			console.log(vm.tracks);
 		});
+		
 	}
 })
